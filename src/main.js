ab5299e75ec76bb2431647870ec5126757254945
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2025-11-13 11:37:55 +0000

    Fix iOS scrolling issues by preventing touchmove and disabling touch-action

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
 
+// Prevent all scrolling on iOS
+document.addEventListener('touchmove', function(e) {
+  e.preventDefault();
+}, { passive: false });
+
 const app = mount(App, {
   target: document.getElementById('app'),
 })
